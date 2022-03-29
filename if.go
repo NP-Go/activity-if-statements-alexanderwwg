5916c8fca1bcf342ef08ad48680f84ab49621e47
Alexander Wong <alexandthemachine@gmail.com>
2022-03-29 11:19:53 +0800

    Put the resultMessasge printing inside the conditionals instead.

--- if.go
+++ if.go
@@ -12,15 +12,16 @@ func main() {
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
 	if number1 < number2 {
-		resultMessage = strconv.Itoa(number2) + " is bigger than " + strconv.Itoa(number1) + "." + "\nThe difference is " + strconv.Itoa(number2-number1)
-
+		resultMessage = strconv.Itoa(number2) + " is bigger than " + strconv.Itoa(number1) + "." + "\nThe difference is " + strconv.Itoa(number2-number1) + "."
+		fmt.Println(resultMessage)
 	}
 	if number1 == number2 {
-		resultMessage = "Both numbers are equal. (" + strconv.Itoa(number1) + ")."
+		resultMessage = "Both numbers are equal. (" + strconv.Itoa(number1) + ")"
+		fmt.Println(resultMessage)
 	}
 	if number1 > number2 {
-		resultMessage = strconv.Itoa(number1) + " is bigger than " + strconv.Itoa(number2) + "." + "\nThe difference is " + strconv.Itoa(number1-number2)
+		resultMessage = strconv.Itoa(number1) + " is bigger than " + strconv.Itoa(number2) + "." + "\nThe difference is " + strconv.Itoa(number1-number2) + "."
+		fmt.Println(resultMessage)
 	}
 
-	fmt.Println(resultMessage)
 }
