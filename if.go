9d026c59cdbb694a6f0d8c146751204f3ef35012
Alexander Wong <alexandthemachine@gmail.com>
2022-03-29 11:26:56 +0800

    Did a greaterThanCheck to compare 2 numbers instead.

--- if.go
+++ if.go
@@ -8,20 +8,22 @@ import (
 func main() {
 	number1 := 17
 	number2 := 24
-	resultMessage := "No outcome."
+
+	greaterThanCheck(number1, number2)
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
+}
+
+func greaterThanCheck(number1 int, number2 int) {
+	resultMessage := "No outcome."
 	if number1 < number2 {
 		resultMessage = strconv.Itoa(number2) + " is bigger than " + strconv.Itoa(number1) + "." + "\nThe difference is " + strconv.Itoa(number2-number1) + "."
-		fmt.Println(resultMessage)
 	}
 	if number1 == number2 {
 		resultMessage = "Both numbers are equal. (" + strconv.Itoa(number1) + ")"
-		fmt.Println(resultMessage)
 	}
 	if number1 > number2 {
 		resultMessage = strconv.Itoa(number1) + " is bigger than " + strconv.Itoa(number2) + "." + "\nThe difference is " + strconv.Itoa(number1-number2) + "."
-		fmt.Println(resultMessage)
 	}
-
+	fmt.Println(resultMessage)
 }
