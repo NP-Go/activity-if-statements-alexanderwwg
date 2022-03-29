a530eefbb615ff990660d8ace810a2a03d63997c
Alexander Wong <alexandthemachine@gmail.com>
2022-03-29 11:10:35 +0800

    using strconv.Itoa + altering resultMessage.

--- if.go
+++ if.go
@@ -1,10 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-	number1 := 17
+	number1 := 55
 	number2 := 24
 	resultMessage := "No outcome."
 	//Insert your code here
 	//Hint: You may wish to make use of strconv.Itoa to convert int to string
+	if number1 < number2 {
+		resultMessage = strconv.Itoa(number2) + " is bigger than " + strconv.Itoa(number1) + "." + "\nThe difference is " + strconv.Itoa(number2-number1)
 
+	}
+	if number1 == number2 {
+		resultMessage = "Both numbers are equal. (" + strconv.Itoa(number1) + ")."
+	}
+	if number1 > number2 {
+		resultMessage = strconv.Itoa(number1) + " is bigger than " + strconv.Itoa(number2) + "." + "\nThe difference is " + strconv.Itoa(number1-number2)
+	}
+
+	fmt.Println(resultMessage)
 }
